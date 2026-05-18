d7bceb9e30f03bcc72580b8fc01ee71ccf20ff88
AkhiatAdem <a3847162@gmail.com>
2026-05-18 16:56:32 +0100

    fix backend

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,11 +84,12 @@ import os
 # Check if we are running in the Azure CI environment
 if os.environ.get('AZURE_PIPELINES') == 'true':
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+    'default': dj_database_url.config(
+        # This reads the environment variable from Render
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
 else:
     DATABASES = {
         'default': {
