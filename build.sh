4fcf7826acb43ce71de4f1268e69e50b2e85e254
AkhiatAdem <a3847162@gmail.com>
2026-05-18 16:51:02 +0100

    fix backend

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files for WhiteNoise
 python manage.py collectstatic --no-input
+
+# Apply database migrations automatically
 python manage.py migrate
--- myproject/settings.py
+++ myproject/settings.py
@@ -21,11 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-9r4z4-!ddy!%a8tm+)y)7xlp%&8i@$466#=4581hc#^#!#pxka'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -45,8 +44,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -149,6 +149,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
